87dc3459e87d219e0801fa0be95c4c254249d63f
naimishStargalaxy <naimish.stargalaxyinfotech@gmail.com>
2024-12-28 12:18:27 +0530

    set chart data

--- lib/Screens/analitics_page.dart
+++ lib/Screens/analitics_page.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: unnecessary_string_interpolations, use_super_parameters, prefer_const_constructors_in_immutables, avoid_print, non_constant_identifier_names, prefer_const_constructors, unnecessary_brace_in_string_interps, prefer_final_fields, unused_field
 
 import 'package:demo_project/Controllers/forex_data_controller.dart';
+import 'package:demo_project/Controllers/home_controller.dart';
 import 'package:demo_project/Data-Model/mock_data.dart';
 import 'package:demo_project/Helper/common_widget.dart';
 import 'package:demo_project/Helper/utility.dart';
@@ -21,14 +22,13 @@ class AnaliticsPageState extends State<AnaliticsPage> {
   late List<ChartSampleData> _chartData;
   late TrackballBehavior _trackballBehavior;
   String saveCurrency = "";
-
   ForexDataController _forexDataController = Get.put(ForexDataController());
+  HomeController _homeController = Get.put(HomeController());
 
   DataGet() async {
     SharedPreferences pref = await SharedPreferences.getInstance();
     setState(() {
       saveCurrency = pref.getString("saveCurrency") ?? "";
-
       print(saveCurrency);
     });
   }
@@ -93,8 +93,9 @@ class AnaliticsPageState extends State<AnaliticsPage> {
                     borderColor: DarkAppColor.bgColor,
                     majorGridLines: MajorGridLines(width: 0)),
                 primaryYAxis: NumericAxis(
-                  minimum: 85,
-                  interval: 5,
+                  minimum: _homeController.valuesData[2]["values"][0],
+                  maximum: _homeController.valuesData[2]["values"][6] + 1,
+                  interval: 1,
                   numberFormat: NumberFormat.compact(),
                   majorGridLines: MajorGridLines(
                       width: 0.5,
@@ -104,16 +105,50 @@ class AnaliticsPageState extends State<AnaliticsPage> {
                   opposedPosition: true,
                   plotBands: [
                     PlotBand(
-                      start: 101,
-                      end: 101,
-                      shouldRenderAboveSeries: false,
-                      borderColor: Colors.blueAccent,
-                      text: "101.0",
+                      start: _homeController.valuesData[2]["values"][6],
+                      end: _homeController.valuesData[2]["values"][3],
+                      shouldRenderAboveSeries: true,
+                      borderColor: Colors.red.withOpacity(.5),
+                      color: Colors.red.withOpacity(0.2),
+                      borderWidth: 2,
+                      text: "Stop 0.0098 (0.98%) 95.8, Ammount 750",
+                      verticalTextAlignment: TextAnchor.start,
+                      horizontalTextAlignment: TextAnchor.middle,
+                      verticalTextPadding: "30",
+                      textStyle: TextStyle(
+                          backgroundColor: Colors.red,
+                          color: DarkAppColor.bgColor,
+                          fontWeight: FontWeight.w700),
+                    ),
+                    PlotBand(
+                      start: _homeController.valuesData[2]["values"][3],
+                      end: _homeController.valuesData[2]["values"][3],
+                      shouldRenderAboveSeries: true,
+                      color: Colors.transparent,
+                      text:
+                          "Closed P&L 0.01437, Qty: 26012, \n Risk/Reward Ratio: 3.46",
+                      verticalTextAlignment: TextAnchor.middle,
+                      horizontalTextAlignment: TextAnchor.middle,
+                      textStyle: TextStyle(
+                          backgroundColor: Colors.green,
+                          color: DarkAppColor.bgColor,
+                          fontWeight: FontWeight.w700),
+                      borderWidth: 2,
+                    ),
+                    PlotBand(
+                      start: _homeController.valuesData[2]["values"][0],
+                      end: _homeController.valuesData[2]["values"][3],
+                      shouldRenderAboveSeries: true,
+                      borderColor: Colors.green.withOpacity(.5),
+                      color: Colors.green.withOpacity(.2),
+                      text: "Target: 0.03314 (3.34%) 331.4, Ammount: 1864.82",
                       verticalTextAlignment: TextAnchor.end,
-                      horizontalTextAlignment: TextAnchor.end,
-                      horizontalTextPadding: "-15",
-                      verticalTextPadding: "5",
-                      textStyle: TextStyle(color: DarkAppColor.primaryColor),
+                      horizontalTextAlignment: TextAnchor.middle,
+                      verticalTextPadding: "30",
+                      textStyle: TextStyle(
+                          backgroundColor: Colors.green,
+                          color: DarkAppColor.bgColor,
+                          fontWeight: FontWeight.w700),
                       borderWidth: 2,
                     )
                   ],
